c9daa90f7156539a070baed14ae435ce4c6441c9
Rithika Sapparapu <rithikasapparapu@tamu.edu>
2026-08-20 04:20:58 -0500

    Add: eating-bananas - submission-4

---
+++ Data Structures & Algorithms/eating-bananas/submission-4.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isfeasible(self, piles, h, k):
+        res = 0
+        for pile in piles:
+            quo = (pile//k)
+            rem = pile%k
+            if rem: res += quo + 1
+            else: res += quo
+        return res <= h
+
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        l, r = 1, max(piles)
+        res = max(piles)
+        while l < r:
+            mid = (l+r)//2
+            if self.isfeasible(piles, h, mid):
+                res = min(res, mid)
+                r = mid
+            else:
+                l = mid + 1
+        return l
+        
+
+        
